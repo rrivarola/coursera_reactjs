06c05ad658b6ecc80860d2a58b35d13ab02c9894
Raúl Rivarola <raul.rivarola@netviax.com>
2020-04-10 00:48:08 -0300

    Task2

--- NodeJS/node-express/index.js
+++ NodeJS/node-express/index.js
@@ -25,24 +25,7 @@ app.use(express.static(__dirname + '/public'));
 
 
 
-app.get('/dishes/:dishId', (req,res,next) => {
-    res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
-});
 
-app.post('/dishes/:dishId', (req, res, next) => {
-  res.statusCode = 403;
-  res.end('POST operation not supported on /dishes/'+ req.params.dishId);
-});
-
-app.put('/dishes/:dishId', (req, res, next) => {
-  res.write('Updating the dish: ' + req.params.dishId + '\n');
-  res.end('Will update the dish: ' + req.body.name + 
-        ' with details: ' + req.body.description);
-});
-
-app.delete('/dishes/:dishId', (req, res, next) => {
-    res.end('Deleting dish: ' + req.params.dishId);
-});
 
 const server = http.createServer(app);
 
