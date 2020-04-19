30cbc6819a8852e644d0c1e4f4fd559359f264d5
Raúl Rivarola <raul.rivarola@netviax.com>
2020-04-19 16:02:43 -0300

    Assigment 3 Task 1 y 2

--- NodeJS/conFusionServer/authenticate.js
+++ NodeJS/conFusionServer/authenticate.js
@@ -8,9 +8,9 @@ var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 
 var config = require('./config.js');
 
-exports.getToken = function(user) {
+exports.getToken = function (user) {
     return jwt.sign(user, config.secretKey,
-        {expiresIn: 3600});
+        { expiresIn: 3600 });
 };
 
 var opts = {};
@@ -20,7 +20,7 @@ opts.secretOrKey = config.secretKey;
 exports.jwtPassport = passport.use(new JwtStrategy(opts,
     (jwt_payload, done) => {
         console.log("JWT payload: ", jwt_payload);
-        User.findOne({_id: jwt_payload._id}, (err, user) => {
+        User.findOne({ _id: jwt_payload._id }, (err, user) => {
             if (err) {
                 return done(err, false);
             }
@@ -33,7 +33,22 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
         });
     }));
 
-exports.verifyUser = passport.authenticate('jwt', {session: false});
+exports.verifyUser = passport.authenticate('jwt', { session: false });
+
+
+
+exports.verifyAdmin = function (req,res,next) {
+    console.log(req.user);
+    if (req.user.admin) {
+        next();
+    }
+    else {
+        var err = new Error('You are not authorized to perform this operation!');
+        err.status = 401;
+        next(err);
+    }
+};
+
 
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
